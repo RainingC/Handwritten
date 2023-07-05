7f8c01f1ba52452591c4a9cb9445d34d6a745b59
曹芮宁 <caoruining@up366.com>
2023-07-05 08:48:32 +0800

    20230704

--- 20、手写实现继承.js
+++ 20、手写实现继承.js
@@ -1,3 +1,4 @@
+// ES6类继承
 class People {
     constructor(name = 'wang', age = '27') {
         this.name = name;
@@ -21,3 +22,27 @@ class Woman extends People {
 }
 let wonmanObj = new Woman('xiaoxiami', '18');
 wonmanObj.eat();
+
+
+// 寄生式组合继承
+function Person(obj) {
+    this.name = obj.name
+    this.age = obj.age
+}
+Person.prototype.add = function(value){
+    console.log(value)
+}
+var p1 = new Person({name:"番茄", age: 18})
+
+function Person1(obj) {
+    Person.call(this, obj) // 偷构造函数式继承
+    this.sex = obj.sex
+}
+// 这一步是继承的关键
+Person1.prototype = Object.create(Person.prototype); // 原型链继承
+Person1.prototype.constructor = Person1;
+
+Person1.prototype.play = function(value){
+    console.log(value)
+}
+var p2 = new Person1({name:"鸡蛋", age: 118, sex: "男"})
